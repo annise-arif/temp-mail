f77e54242824134d791d0d08bf0c3f61b558854d
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2023-03-04 04:34:41 +0600

    completed temp mail logic code writing

--- src/App.js
+++ src/App.js
@@ -1,46 +1,77 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [allMail, setAllmail] = useState([]);
+  console.log(allMail);
 
-
-    const handleEmail = () => {
-      const domains = ["gmail.com"];
-      const usernameLength = Math.floor(Math.random() * (12 - 6 + 1) + 6);
-      const username = Array.from({ length: usernameLength }, () => {
-        return String.fromCharCode(
-          Math.floor(Math.random() * (122 - 97 + 1) + 97)
-        );
-      }).join("");
-      const domain = domains[Math.floor(Math.random() * domains.length)];
-      const email = `${username}@${domain}`;
-
-      document.getElementById("email-display").innerHTML = email; // display the generated email in the email display
-    }
-
-    const copyContent = async () => {
-      let text = document.getElementById('email-display').innerHTML;
-      try {
-        await navigator.clipboard.writeText(text);
-        console.log('Content copied to clipboard');
-      } catch (err) {
-        console.error('Failed to copy: ', err);
+  useEffect(() => {
+    fetch(
+      "https://api.testmail.app/api/json?apikey=87384f5c-db35-4c17-8efa-e43eae70fd4e&namespace=cibce&pretty=true",
+      {
+        headers: {
+          Authorization: "Bearer 87384f5c-db35-4c17-8efa-e43eae70fd4e",
+        },
       }
+    )
+      .then((res) => res.json())
+      .then((data) => setAllmail(data.emails));
+  }, []);
+
+  const handleEmail = () => {
+    const domains = ["inbox.testmail.app"];
+    const usernameLength = Math.floor(Math.random() * (12 - 6 + 1) + 6);
+    const username = Array.from({ length: usernameLength }, () => {
+      return String.fromCharCode(
+        Math.floor(Math.random() * (122 - 97 + 1) + 97)
+      );
+    }).join("");
+    const domain = domains[Math.floor(Math.random() * domains.length)];
+    const email = `cibce.${username}@${domain}`;
+
+    document.getElementById("email-display").innerHTML = email; // display the generated email in the email display
+  };
+
+  const copyContent = async () => {
+    let text = document.getElementById("email-display").innerHTML;
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("Content copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
     }
+  };
 
   return (
     <div className="App">
-      <h1>header</h1>
+      <h1 style={{backgroundColor: "gray", padding: "20px 0px 20px 0px", marginTop: "0px"}}>header</h1>
       <br />
-      <hr />
       <main>
         <button onClick={handleEmail}>Generate Email</button>
         <div>
-        <div id="email-display" style={{margin: "15px 0px 15px 0px"}}></div> <button onClick={copyContent}>Copy Email</button>
+          <div id="email-display" style={{ margin: "15px 0px 15px 0px" }}></div>{" "}
+          <button onClick={copyContent}>Copy Email</button>
+        </div>
+
+        <div>
+          <h3 style={{textDecoration: "underline", marginTop: "52px"}}>All Mail</h3>
+          <div>
+            {allMail?.map((m, i) => {
+              return (
+                <div key={i}>
+                  <p>Name: {m.from_parsed[0].name}</p>
+                  <p>From: {m.from_parsed[0].address}</p>
+                  <div>Subject: {m.subject}</div>
+                  <hr />
+                </div>
+                
+              );
+            })}
+          </div>
         </div>
       </main>
       <br />
-      <hr />
-      <footer>footer</footer>
+      <footer style={{backgroundColor: "gray"}}>footer</footer>
     </div>
   );
 }
