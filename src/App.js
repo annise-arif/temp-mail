181cd70a786f859f86ce80f5acaf3fa39d1cf7ff
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2023-03-04 00:04:04 +0600

    emplemented a random email by clicking a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+
+
+    const handleEmail = () => {
+      const domains = ["gmail.com"];
+      const usernameLength = Math.floor(Math.random() * (12 - 6 + 1) + 6);
+      const username = Array.from({ length: usernameLength }, () => {
+        return String.fromCharCode(
+          Math.floor(Math.random() * (122 - 97 + 1) + 97)
+        );
+      }).join("");
+      const domain = domains[Math.floor(Math.random() * domains.length)];
+      const email = `${username}@${domain}`;
+
+      document.getElementById("email-display").innerHTML = email; // display the generated email in the email display
+    }
+
+    const copyContent = async () => {
+      let text = document.getElementById('email-display').innerHTML;
+      try {
+        await navigator.clipboard.writeText(text);
+        console.log('Content copied to clipboard');
+      } catch (err) {
+        console.error('Failed to copy: ', err);
+      }
+    }
+
   return (
     <div className="App">
       <h1>header</h1>
-      <main>main</main>
+      <br />
+      <hr />
+      <main>
+        <button onClick={handleEmail}>Generate Email</button>
+        <div>
+        <div id="email-display" style={{margin: "15px 0px 15px 0px"}}></div> <button onClick={copyContent}>Copy Email</button>
+        </div>
+      </main>
+      <br />
+      <hr />
       <footer>footer</footer>
     </div>
   );
